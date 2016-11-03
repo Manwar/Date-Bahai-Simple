dd0f3f42ab69a8e1d8044148633cb6faa282a820
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-11-03 09:41:47 +0000

    - Added key 'provides' to the Makefile.PL script.

--- lib/Date/Bahai/Simple.pm
+++ lib/Date/Bahai/Simple.pm
@@ -1,6 +1,6 @@
 package Date::Bahai::Simple;
 
-$Date::Bahai::Simple::VERSION   = '0.15';
+$Date::Bahai::Simple::VERSION   = '0.16';
 $Date::Bahai::Simple::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Date::Bahai::Simple - Represents Bahai date.
 
 =head1 VERSION
 
-Version 0.15
+Version 0.16
 
 =cut
 
